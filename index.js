f7b3fea95b85c06bc0659abac1fee5a76349f537
Seia-Soto <seia@outlook.kr>
2020-03-18 09:22:17 +0900

    setup the JSON api structure

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Koa = require('koa')
+const useJSON = require('koa-json')
 const debug = require('debug')
 
+const routers = require('./routers')
 const config = require('./config')
 const pkg = require('./package')
 
@@ -11,6 +13,9 @@ app.context.log = log
 
 const initFn = async () => {
   app
+    .use(useJSON({ pretty: false }))
+    .use(routers.routes())
+    .use(routers.allowedMethods())
     .listen(config.app.port, () => log('`yeonit` API server is starting'))
 }
 
