a836bad9fd33ad39bac3f793e3e929ea64a094c0
Seia-Soto <seia@outlook.kr>
2020-06-21 21:28:09 +0900

    Create util fn to create logger

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 const Koa = require('koa')
-const debug = require('debug')
 const { v1 } = require('uuid')
 
 const routers = require('./routers')
 const config = require('./config')
-const pkg = require('./package')
+const log = require('./log')
 
 const app = new Koa()
-const log = debug(pkg.name)
 
 if (!config.app.key || !config.app.key.length) {
   log('the token keys will set as randomized value with uuid format because it was not provided in config')
 
   app.key = v1()
 }
-
-app.context.log = log
 
 const initFn = async () => {
   app
